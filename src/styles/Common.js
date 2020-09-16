451bb7e4e0362cb34d3b68f890e34a3344b690b1
helderdiin <helderdiin.work@gmail.com>
2020-09-16 19:51:17 -0300

    Adicionando estilos padrões para reutilização

---
+++ src/styles/Common.js
@@ -0,0 +1,6 @@
+export const GenericContainer = {
+  display: 'flex',
+  width: '100%',
+  marginTop: '10px',
+  padding: '0 15px 10px',
+};
---
+++ src/styles/SectorDivider.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export default styled.div`
+  width: 100%;
+  border-bottom: 1px solid rgb(189, 189, 189);
+  position: absolute;
+`;
