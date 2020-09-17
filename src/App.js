a7c4182bb79eb0e3756f973af7fd5ccb3616cc30
helderdiin <helderdiin.work@gmail.com>
2020-09-16 22:25:01 -0300

    Adicionando background dinâmico

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
+import moment from 'moment';
 
 import './global.css';
 
@@ -11,8 +12,12 @@ import HourlyWeather from './components/HourlyWeather';
 import DailyWeather from './components/DailyWeather';
 import TodayDescription from './components/TodayDescription';
 
+import AfternoonImg from './assets/after_noon.png';
+import NightImg from './assets/night.png';
+
 function App() {
   const dispatch = useDispatch();
+  const [backgroundImage, setBackgroundImage] = useState(AfternoonImg);
 
   useEffect(() => {
     const watchId = navigator.geolocation.watchPosition(async ({ coords }) => {
@@ -31,8 +36,18 @@ function App() {
     return () => navigator.geolocation.clearWatch(watchId);
   }, [dispatch]);
 
+  useEffect(() => {
+    const currentHour = +moment().format('HH');
+
+    if (currentHour > 6 && currentHour < 18) {
+      setBackgroundImage(AfternoonImg);
+    } else {
+      setBackgroundImage(NightImg);
+    }
+  }, []);
+
   return (
-    <div id="app">
+    <div id="app" style={{ backgroundImage: `url(${backgroundImage})` }}>
       <MainWeather />
       <HourlyWeather />
       <DailyWeather />
