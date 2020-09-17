0cecbf6eed73f3345c2b674ff8462aff3bb80898
helderdiin <helderdiin.work@gmail.com>
2020-09-16 22:10:33 -0300

    Integrando o componente de descrição do dia

--- src/components/TodayDescription/index.jsx
+++ src/components/TodayDescription/index.jsx
@@ -1,14 +1,29 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 import SectorDivider from '../../styles/SectorDivider';
 import Container from './styles';
 
 function TodayDescription() {
+  const cityStatus = useSelector((state) => state.weather.cityStatus);
+  const temp = useSelector((state) => state.weather.temp);
+  const tempMax = useSelector((state) => state.weather.tempMax);
+
   return (
     <>
       <SectorDivider />
       <Container>
-        Hoje: 'colocar a descrição da API'. A temperatura é de XX°; a máxima hoje foi prevista como YY°.
+        Hoje:
+        {' '}
+        {cityStatus}
+        {' '}
+        no momento. A temperatura é de
+        {' '}
+        {temp}
+        °; a máxima hoje foi prevista como
+        {' '}
+        {tempMax}
+        °.
       </Container>
     </>
   );
